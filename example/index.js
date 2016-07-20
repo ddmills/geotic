da92befff339d83a14e807a0f3d232b653586087
ddmills <dmills@iastate.edu>
2016-07-20 18:40:47 -0500

    cleanup example

--- example/index.js
+++ example/index.js
@@ -12,11 +12,11 @@ component('hair', (entity) => {
   };
 });
 
-
-const dog = entity() // create a new entity
-  .add('hair') // add a hair component
+//define entities
+const dog = entity()
+  .add('hair')
   .add('name', 'Sam') // pass arguments to components
-  .tag('animal') // tags can be used for many things
+  .tag('animal')
   .add('pos');
 
 const cat = entity()
@@ -25,22 +25,14 @@ const cat = entity()
   .add('hair')
   .add('pos');
 
-// reference components by "entity.c[component-name]"
 cat.c.name; // "Princess Dilly"
-
-// set properties on components
 cat.c.pos.x = 20;
-
-// all entities have an id (integer)
 cat.id;
 
-// get all entities which have a 'name' and 'hair' component
 geotic.findByComponent('name', 'hair');  // [cat, dog]
 
-// remove a component from an entity
-cat.add('hair');
+cat.remove('hair');
 
 geotic.findByComponent('name', 'hair'); // [dog]
 
-// get all entities with given tags
 geotic.findByTag('animal');  // [cat, dog]
