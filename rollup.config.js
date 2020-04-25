7db739f5d4d3d8472790a79d68b024fbc6fa572c
ddmills <mills.dalton@gmail.com>
2020-04-25 17:32:28 -0500

    add terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
     input: 'src/index.js',
@@ -8,6 +9,7 @@ export default {
         dir: 'build',
         name: 'geotic',
         format: 'es',
+        sourcemap: true,
     },
-    plugins: [babel(), commonjs(), resolve({ browser: true })],
+    plugins: [babel(), commonjs(), resolve({ browser: true }), terser()],
 };
